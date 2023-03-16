d226119252ec49237641f493dbf1dba61c27c9d0
mrpriyansh <mr.priyanshgaharana@gmail.com>
2023-03-16 19:52:41 +0530

    Add try except block to handle error

--- app.py
+++ app.py
@@ -31,41 +31,49 @@ def home():
 
 @app.route('/add_contact', methods=["POST"])
 def add_contact():
-    request_data = request.get_json()
+	try:
+		request_data = request.get_json()
 
-    required_field = ["phone_no", "phone_ext"]
+		required_field = ["phone_no", "phone_ext"]
 
-    for field in required_field:
-        if request_data[field] is None:
-             response = field +" is required!"
-             return response , 400
+		for field in required_field:
+			if request_data[field] is None:
+				response = field +" is required!"
+				return response , 400
 
-    first_name = request_data['first_name']
-    middle_name = request_data['middle_name']
-    last_name = request_data['last_name']
-    phone_no = request_data['phone_no']
-    phone_ext = request_data['phone_ext']
+		first_name = request_data['first_name']
+		middle_name = request_data['middle_name']
+		last_name = request_data['last_name']
+		phone_no = request_data['phone_no']
+		phone_ext = request_data['phone_ext']
 
-    contact = Contact(first_name=first_name, middle_name=middle_name, last_name=last_name, phone_no=phone_no, phone_ext=phone_ext)
+		contact = Contact(first_name=first_name, middle_name=middle_name, last_name=last_name, phone_no=phone_no, phone_ext=phone_ext)
 
-    db.session.add(contact)
-    db.session.commit()
-    return "Contact Added Successfully"
+		db.session.add(contact)
+		db.session.commit()
+		return "Contact Added Successfully"
+	except:
+		return "Server Error", 500
+
+    
 
 
 @app.route('/delete_contact', methods=["DELETE"])
 def delete_contact():
-    id = request.args.get('id')
+	try:
+		id = request.args.get('id')
 
-    if id is None:
-        return "Contact Id is required", 400
+		if id is None:
+			return "Contact Id is required", 400
 
-    contact = Contact.query.filter_by(id=id).first()
-    if contact is None:
-        return "No Contact Found", 404
-    db.session.delete(contact)
-    db.session.commit()
-    return "Contact Deleted Successfully"
+		contact = Contact.query.filter_by(id=id).first()
+		if contact is None:
+			return "No Contact Found", 404
+		db.session.delete(contact)
+		db.session.commit()
+		return "Contact Deleted Successfully"
+	except:
+		return "Server Error", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
